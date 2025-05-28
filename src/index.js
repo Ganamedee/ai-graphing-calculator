251ca8bacfa906cce642de475fe5e1f14568d777
Ganamede <tolenaardaniel@gmail.com>
2025-05-28 23:43:26 +0100

    Add error handling

--- src/index.js
+++ src/index.js
@@ -41,6 +41,13 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+// Error handler to always return JSON
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal Server Error" });
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
